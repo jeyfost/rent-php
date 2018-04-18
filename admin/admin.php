8e1c59ae18e98bfe44cda8434a6152581d6de664
jeyfost <foster_andrew@tut.by>
2018-04-18 12:40:31 +0300

    Добавлена возможность выхода из панели администрирования

--- admin/admin.php
+++ admin/admin.php
@@ -93,6 +93,11 @@ if($_SESSION['userID'] != 1) {
             <i class="fa fa-truck" aria-hidden="true"></i><span> Прицепы</span>
         </div>
     </a>
+    <a href="/admin/security/">
+        <div class="menuPoint">
+            <i class="fa fa-shield" aria-hidden="true"></i><span> Безопасность</span>
+        </div>
+    </a>
 </div>
 
 <div id="content">
---
+++ scripts/admin/ajaxExit.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeyfost
+ * Date: 18.04.2018
+ * Time: 12:39
+ */
+
+session_start();
+unset($_SESSION['userID']);
